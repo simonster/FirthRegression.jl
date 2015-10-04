f3951f12eaf7fb2df3d7ae44ebcc74cd729ebcbd
Simon Kornblith <simon@simonster.com>
2015-10-04 15:08:39 -0400

    Implement Firth ANOVA

--- src/FirthRegression.jl
+++ src/FirthRegression.jl
@@ -2,7 +2,7 @@ module FirthRegression
 using Reexport, Compat
 @reexport using GLM
 using GLM: wrkwt!, installbeta!
-export FirthGLM, penalized_deviance
+export FirthGLM, penalized_deviance, anova
 
 mueta2(::LogitLink, η) = (e = exp(η); (e*(1-e))/(1+e)^3)
 mueta2(::LogLink, η) = exp(η)
@@ -19,17 +19,43 @@ type FirthGLM{G<:FirthGlmResp,L<:LinPred} <: GLM.AbstractGLM
     fit::Bool
 end
 
+type FirthGLMTest{G<:FirthGlmResp,L<:LinPred} <: GLM.AbstractGLM
+    rr::G
+    pp::L
+    pp_full::L
+    fit::Bool
+end
+
+function updatefact!{T}(p::GLM.DensePredChol{T}, wt::Vector{T})
+    scr = scale!(p.scratch, wt, p.X)
+    cholfact!(At_mul_B!(GLM.cholfactors(p.chol), scr, p.X), :U)
+end
+
+function updatefact!{T}(p::GLM.SparsePredChol{T}, wt::Vector{T})
+    scr = scale!(p.scratch, wt, p.X)
+    XtX = p.Xt*scr
+    c = p.chol = $(if VERSION >= v"0.4.0-dev+3307"
+        :(cholfact(Symmetric{eltype(XtX),typeof(XtX)}(XtX, 'U')))
+    else
+        :(cholfact(scale!(XtX + XtX', convert(eltype(XtX), 1/2))))
+    end)
+end
+
 # TODO make this more efficient
 hatdiag(p::GLM.DensePredChol, wt::Vector) = sum(p.X.*(p.chol\p.scratch')', 2)
+hatdiag(p::GLM.SparsePredChol, wt::Vector) = sum(p.X.*(p.chol\p.Xt)', 2).*wt
 
-function adjustwrkresid!(m::FirthGLM, wt::Vector)
+hatdiag(m::FirthGLM, wt::Vector) = hatdiag(m.pp, wt)
+hatdiag(m::FirthGLMTest, wt::Vector) = hatdiag(m.pp_full, wt)
+
+function adjustwrkresid!(m::@compat(Union{FirthGLM,FirthGLMTest}), wt::Vector)
     r = m.rr
     link = r.l
     eta = r.eta
     mueta = r.mueta
     var = r.var
     wrkresid = r.wrkresid
-    h = hatdiag(m.pp, wt)
+    h = hatdiag(m, wt)
     @inbounds @simd for i = 1:length(eta)
         # See Kosmidis and Firth, 2009, 4⋅3
         wrkresid[i] += h[i]*mueta2(link, eta[i])/(2*mueta[i]^3/var[i])
@@ -37,7 +63,13 @@ function adjustwrkresid!(m::FirthGLM, wt::Vector)
     r
 end
 
-function GLM._fit(m::FirthGLM, verbose::Bool, maxIter::Integer, minStepFac::Real,
+penalized_deviance(m::FirthGLM) = deviance(m.rr) - logdet(m.pp.chol)
+function penalized_deviance(m::FirthGLMTest)
+    updatefact!(m.pp_full, m.rr.wrkwts)
+    deviance(m.rr) - logdet(m.pp_full.chol)
+end
+
+function GLM._fit!(m::@compat(Union{FirthGLM,FirthGLMTest}), verbose::Bool, maxIter::Integer, minStepFac::Real,
                   convTol::Real, start)
     m.fit && return m
     maxIter >= 1 || error("maxIter must be positive")
@@ -48,10 +80,14 @@ function GLM._fit(m::FirthGLM, verbose::Bool, maxIter::Integer, minStepFac::Real
     if start != nothing
         copy!(p.beta0, start)
         fill!(p.delbeta, 0)
-    else
-        delbeta!(p, wrkresp(r), wrkwt!(r))
-        installbeta!(p)
+        linpred!(lp, p)
+        updatemu!(r, lp)
     end
+    delbeta!(p, wrkresp(r), wrkwt!(r))
+    if isa(m, FirthGLMTest)
+        updatefact!(m.pp_full, m.rr.wrkwts)
+    end
+    installbeta!(p)
     oldwrkwts = similar(r.wrkwts)
     devold = Inf
     for i=1:maxIter
@@ -89,6 +125,84 @@ function GLM._fit(m::FirthGLM, verbose::Bool, maxIter::Integer, minStepFac::Real
     m
 end
 
-penalized_deviance(m::FirthGLM) = deviance(m.rr) - logdet(m.pp.chol)
+function adjustwrkresid!(m::FirthGLMTest, wt::Vector)
+    r = m.rr
+    link = r.l
+    eta = r.eta
+    mueta = r.mueta
+    var = r.var
+    wrkresid = r.wrkresid
+    h = hatdiag(m.pp_full, wt)
+    @inbounds @simd for i = 1:length(eta)
+        # See Kosmidis and Firth, 2009, 4⋅3
+        wrkresid[i] += h[i]*mueta2(link, eta[i])/(2*mueta[i]^3/var[i])
+    end
+    r
+end
+
+cols(X::AbstractMatrix) = [sub(X, :, i) for i = 1:size(X, 2)]
+cols(X::SparseMatrixCSC) = [X[:, i] for i = 1:size(X, 2)]
+
+immutable ANOVAResult
+    chisq::Float64
+    df::Int
+    p::Float64
+end
+
+"""
+Performs analysis of deviance for two nested design matrices with
+a Firth penalized approach. The penalty for both models is determined
+based on the Fisher information matrix for the larger model.
+"""
+function anova(m1::FirthGLM, m2::FirthGLM; fitargs...)
+    m1.rr.y == m2.rr.y || throw(ArgumentError("model responses (y) do not match"))
+    (m1.rr.d == m2.rr.d && m1.rr.l == m2.rr.l) ||
+        throw(ArgumentError("model distributions or link functions do not match"))
+    m1.rr.wts == m2.rr.wts || throw(ArgumentError("model weights do not match"))
+    m1.rr.offset == m2.rr.offset || throw(ArgumentError("model offsets do not match"))
+
+    if size(m1.pp.X, 2) == size(m2.pp.X, 2)
+        throw(ArgumentError("one design matrix must be nested inside the other"))
+    elseif size(m1.pp.X, 2) > size(m2.pp.X, 2)
+        full, reduced = m1, m2
+    else
+        full, reduced = m2, m1
+    end
+
+    # Figure out which columns are nested
+    fullcols = cols(full.pp.X)
+    reduced_colidx = findin(fullcols, cols(reduced.pp.X))
+    if length(reduced_colidx) != size(reduced.pp.X, 2)
+        throw(ArgumentError("one design matrix must be nested inside the other"))
+    end
+
+    # Fit the full model if necessary
+    if !full.fit
+        fit!(full; fitargs...)
+    end
+    fulldev = penalized_deviance(full)
+
+    # Fit the reduced model
+    if !reduced.fit
+        # If the second model has not been fit, we can reuse its
+        # response and linear predictor
+        rr = reduced.rr
+        pp = reduced.pp
+        startcoef = coef(full)[reduced_colidx]
+    else
+        rr = typeof(reduced.rr)(reduced.rr.y, reduced.rr.d, reduced.rr.l,
+                                copy(reduced.rr.eta), similar(reduced.rr.y),
+                                reduced.rr.offset, reduced.rr.wts)
+        pp = GLM.cholpred(reduced.pp.X)
+        startcoef = coef(reduced)
+    end
+    reducedmodel = FirthGLMTest(rr, pp, GLM.cholpred(full.pp.X), false)
+    fit!(reducedmodel; start=startcoef, fitargs...)
+    reduceddev = penalized_deviance(reducedmodel)
+
+    chisq = reduceddev - fulldev
+    df = size(full.pp.X, 2) - length(reduced_colidx)
+    ANOVAResult(chisq, df, ccdf(Chisq(df), chisq))
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,34 @@
 using FirthRegression, Base.Test, DataFrames
 
 ## Firth regression with admit data
-Base.disable_threaded_libs()
 df = readtable(Pkg.dir("GLM","data","admit.csv.gz"))
 df[:rank] = pool(df[:rank])
 gm = fit(FirthGLM, admit ~ gre + gpa + rank, df, Binomial(), convTol=1e-14)
 @test_approx_eq_eps coef(gm) [-3.9076341278382402677, 0.0022173644223770295, 0.7875352644980999628, -0.6657797116943170446, -1.3177582553490327921, -1.5111669280800947845] 1e-8
 @test_approx_eq penalized_deviance(gm.model) 431.06681398743257
+
+## With sparse matrices
+srand(1)
+X = sprand(1000, 10, 0.01)
+β = randn(10)
+y = Bool[rand() < x for x in logistic(X * β)]
+
+gmdense = fit(FirthGLM, full(X), y, Binomial())
+gmsparse = fit(FirthGLM, X, y, Binomial())
+@test_approx_eq deviance(gmsparse) deviance(gmdense)
+@test_approx_eq coef(gmsparse) coef(gmdense)
+
+## ANOVA
+anv = anova(fit(FirthGLM, admit ~ gre + gpa + rank, df, Binomial(), convTol=1e-14).model,
+            fit(FirthGLM, admit ~ gre + gpa, df, Binomial(), convTol=1e-14).model, convTol=1e-14)
+@test_approx_eq anv.chisq 21.373030365595071
+@test anv.df == 3
+@test_approx_eq anv.p 8.8071001596912168e-05
+
+## ANOVA w/o prefit models
+anv = anova(fit(FirthGLM, admit ~ gre + gpa + rank, df, Binomial(), convTol=1e-14, dofit=false).model,
+            fit(FirthGLM, admit ~ gre + gpa, df, Binomial(), convTol=1e-14, dofit=false).model, convTol=1e-14)
+@test_approx_eq anv.chisq 21.373030365595071
+@test anv.df == 3
+@test_approx_eq anv.p 8.8071001596912168e-05
+
